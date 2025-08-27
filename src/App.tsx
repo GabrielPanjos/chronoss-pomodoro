0bf6dceafe5e7629a9ead2320fbd01bfb712b28d
Gabriel Pereira <pereiradosanjosgabriel@gmail.com>
2025-08-26 21:55:17 -0300

    estruturando o HTML e o CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Heading } from "./components/Heading";
-import { TimerIcon } from "lucide-react";
 
 export function App() {
   return (
-    <div>
-      <Heading>
-        Olá mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti saepe
-        sunt labore cum. Repellat voluptas, nihil iste repudiandae ab porro
-        magni alias reiciendis, suscipit corrupti veniam est iusto similique
-        sint.
-      </p>
-    </div>
+    <>
+      <div className="container">
+        <div className="content">
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
+    </>
   );
 }
