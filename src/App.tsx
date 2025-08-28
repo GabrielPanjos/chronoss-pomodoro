999e1b718c8a84fd11b764ba8d611d4f9dd8b786
Gabriel Pereira <pereiradosanjosgabriel@gmail.com>
2025-08-28 15:10:13 -0300

    criando componente de CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Container } from "./components/Container";
-import { Heading } from "./components/Heading";
-import { Logo } from "./components/Logo";
-
 import "./styles/theme.css";
 import "./styles/global.css";
+
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 export function App() {
   return (
@@ -13,15 +13,7 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
-      </Container>
-
-      <Container>
-        <Heading>FORM</Heading>
-      </Container>
-
-      <Container>
-        <Heading>FOOTER</Heading>
+        <Menu />
       </Container>
     </>
   );
